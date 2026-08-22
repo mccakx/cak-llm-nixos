a801f5a0f8a5dc8070b11592a7952a5443c8ead7
cak <cak@local>
2026-08-22 21:21:49 +0700

    fonts: noto-fonts-emoji -> noto-fonts-color-emoji (renamed)

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -5,7 +5,7 @@
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       jetbrains-mono
       nerd-fonts.jetbrains-mono
     ];
